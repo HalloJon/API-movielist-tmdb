fea27eb48cde27b2d59265ae5984b1904acd46fd
HalloJon <127466597+HalloJon@users.noreply.github.com>
2025-02-10 14:24:45 +0700

    Add files via upload

---
+++ conf/info.php
@@ -0,0 +1,12 @@
+<?php
+
+$apikey = "af7d8bb504ee423ac545c25a696c4cc1";
+
+$sitename = "Film List Update";
+$tagline = "Cari movie & tv di sini aja";
+
+
+$imgurl_1 = "http://image.tmdb.org/t/p/w500";
+$imgurl_2 = "http://image.tmdb.org/t/p/w300";
+
+?>
---
+++ footer.php
@@ -0,0 +1,5 @@
+
+          <h2><?php echo $sitename ?></h3>
+          <p>Copyright &copy; 2025 - developed by <a href="http://jonathan.id">Jon</a></p>
+  </body>
+</html>
